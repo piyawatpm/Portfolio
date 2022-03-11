c48d5a02d3ed4788c4fe38900c9f3267700ef452
Piyawat <piyawat_00101@hotmail.com>
2022-03-11 16:02:07 +1100

    pagination

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,23 +1,53 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Title from "../../components/Title";
 
 // be title, url, imageUrl, summary
-function NewsPage({ article }: any) {
+function NewsPage({ article, pageNo }: any) {
   const router = useRouter();
+  const [pages, setPages] = useState(pageNo);
+  const [data, setData] = useState(article);
   const handleHome = () => {
     router.push("/");
-    console.log("test");
     console.log(article);
   };
   console.log(article);
+  useEffect(() => {
+    router.push(`/news?pages=${pages}`, undefined, { shallow: true });
+    console.log("useEffect");
+  }, [pages]);
 
+  const fetchNewData = async () => {
+    const response = await fetch(
+      `https://api.spaceflightnewsapi.net/v3/articles?_limit=10&_start=${
+        pages * 10
+      }`
+    );
+    const data = await response.json();
+    
+    setData(data);
+  };
+  const handleNextPage =  () => {
+    setPages((page: number) => page + 1);
+     fetchNewData();
+  };
+  const handlePreviousPage =  () => {
+    setPages((page: number) => page - 1);
+     fetchNewData();
+  };
   return (
     <div>
       <button onClick={handleHome} style={{ color: "red" }}>
         Home
       </button>
       <h1>NewsPage</h1>
-      {article.map((each: any) => {
+      <div className="flex flex-row">
+        <button onClick={handlePreviousPage}>{"<===="}</button>
+        <h1>{pages}</h1>
+        <button onClick={handleNextPage}>{"====>"}</button>
+      </div>
+
+      {data.map((each: any) => {
         return (
           <Title
             key={each.id}
@@ -35,14 +65,23 @@ function NewsPage({ article }: any) {
 
 export default NewsPage;
 
-export async function getServerSideProps() {
-  const response = await fetch(
-    "https://api.spaceflightnewsapi.net/v3/articles?_limit=50"
-  );
+export async function getServerSideProps(context: any) {
+  const { query } = context;
+  const { pages } = query;
+  const pageNo = pages ? pages : "1";
+  const url = pages
+    ? `https://api.spaceflightnewsapi.net/v3/articles?_limit=10&_start=${
+        (parseInt(pageNo) - 1) * 10
+      }`
+    : `https://api.spaceflightnewsapi.net/v3/articles?_limit=10`;
+  console.log(pageNo);
+
+  const response = await fetch(url);
   const data = await response.json();
   return {
     props: {
       article: data,
+      pageNo: parseInt(pageNo),
     },
     // revalidate: 360 //should be regenerate every 1 hr
     // notFound:true
